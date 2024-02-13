f3c9ca7e3f8ec95de805391f3873b1ee78b67fe0
Matthew Davies <70969486+mfdavies@users.noreply.github.com>
2024-02-13 12:28:28 -0700

    Camera Transitions Done

---
+++ Dust Bunny/Assets/Scripts/Cameras/CameraContolTrigger.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEditor;
+using UnityEngine;
+
+public class CameraContolTrigger : MonoBehaviour
+{
+
+    public CustomInspectorObjects customInspectorObjects;
+    private Collider2D _col;
+    void Awake()
+    {
+        _col = GetComponent<Collider2D>();
+    } // end Awake
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (customInspectorObjects.PanCameraOnContact)
+            {
+                CameraManager.instance.PanCameraOnContact(customInspectorObjects.panDistance, customInspectorObjects.panTime, customInspectorObjects.panDirection, false);
+            }
+        }
+    } // end OnTriggerEnter2D
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Vector2 exitDirection = (other.transform.position - _col.bounds.center).normalized;
+            if (customInspectorObjects.SwapCameras && customInspectorObjects.cameraOnLeft != null && customInspectorObjects.cameraOnRight != null)
+            {
+                CameraManager.instance.SwapCamera(customInspectorObjects.cameraOnLeft, customInspectorObjects.cameraOnRight, exitDirection);
+            }
+
+            if (customInspectorObjects.PanCameraOnContact)
+            {
+                CameraManager.instance.PanCameraOnContact(customInspectorObjects.panDistance, customInspectorObjects.panTime, customInspectorObjects.panDirection, true);
+            }
+        }
+    } // end OnTriggerExit2D
+} // end class CameraContolTrigger
+
+[System.Serializable]
+public class CustomInspectorObjects
+{
+    public bool SwapCameras = false;
+    public bool PanCameraOnContact = false;
+    [HideInInspector] public CinemachineVirtualCamera cameraOnLeft;
+    [HideInInspector] public CinemachineVirtualCamera cameraOnRight;
+    [HideInInspector] public PanDirection panDirection;
+    [HideInInspector] public float panDistance = 3f;
+    [HideInInspector] public float panTime = 0.35f;
+} // end class CustomInspectorObjects
+
+public enum PanDirection
+{
+    Up,
+    Down,
+    Left,
+    Right
+} // end enum PanDirecrtion
+
+[CustomEditor(typeof(CameraContolTrigger))]
+public class MyScriptEditor : Editor
+{
+    CameraContolTrigger CameraContolTrigger;
+    private void OnEnable()
+    {
+        CameraContolTrigger = (CameraContolTrigger)target;
+    } // end OnEnable
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        if (CameraContolTrigger.customInspectorObjects.SwapCameras)
+        {
+            CameraContolTrigger.customInspectorObjects.cameraOnLeft = (CinemachineVirtualCamera)EditorGUILayout.ObjectField("Camera On Left", CameraContolTrigger.customInspectorObjects.cameraOnLeft, typeof(CinemachineVirtualCamera), true);
+            CameraContolTrigger.customInspectorObjects.cameraOnRight = (CinemachineVirtualCamera)EditorGUILayout.ObjectField("Camera On Right", CameraContolTrigger.customInspectorObjects.cameraOnRight, typeof(CinemachineVirtualCamera), true);
+        }
+        if (CameraContolTrigger.customInspectorObjects.PanCameraOnContact)
+        {
+            CameraContolTrigger.customInspectorObjects.panDirection = (PanDirection)EditorGUILayout.EnumPopup("Pan Direction", CameraContolTrigger.customInspectorObjects.panDirection);
+            CameraContolTrigger.customInspectorObjects.panDistance = EditorGUILayout.FloatField("Pan Distance", CameraContolTrigger.customInspectorObjects.panDistance);
+            CameraContolTrigger.customInspectorObjects.panTime = EditorGUILayout.FloatField("Pan Time", CameraContolTrigger.customInspectorObjects.panTime);
+        }
+
+        if (GUI.changed)
+        {
+            EditorUtility.SetDirty(CameraContolTrigger);
+        }
+    } // end OnInspectorGUI
+} // end class MyScriptEditor
---
+++ Dust Bunny/Assets/Scripts/Cameras/CameraManager.cs
@@ -0,0 +1,163 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public static CameraManager instance;
+    [SerializeField] private CinemachineVirtualCamera[] _allVirtualCameras;
+
+    [Header("Controls for lerping the Y Damping during player jump/fall")]
+    [SerializeField] private float _fallPanAmount = 0.25f;
+    [SerializeField] private float _fallYPanTime = 0.35f;
+    public float _fallSpeedYDampingChangeThreshold = -15f;
+    public bool IsLerpingYDamping { get; private set; }
+    public bool LerpedFromPlayerFalling { get; set; }
+    private Coroutine _lerpYPanCoroutine;
+    private Coroutine _panCameraCoroutine;
+    private CinemachineVirtualCamera _currentCamera;
+    private CinemachineFramingTransposer _framingTransposer;
+    private float _normYPanAmount;
+    private Vector2 _startingTrackedObjectOffset;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        for (int i = 0; i < _allVirtualCameras.Length; i++)
+        {
+            if (_allVirtualCameras[i].enabled)
+            {
+                _currentCamera = _allVirtualCameras[i];
+                _framingTransposer = _currentCamera.GetCinemachineComponent<CinemachineFramingTransposer>();
+            }
+        }
+        // Set the YDamping amount so it's based on the inspector value
+        _normYPanAmount = _framingTransposer.m_YDamping;
+
+        // Set the starting position of the tracked object offset
+        _startingTrackedObjectOffset = _framingTransposer.m_TrackedObjectOffset;
+    } // end Awake
+
+    #region Lerp the Y Damping
+    public void LerpYDamping(bool isPlayerFalling)
+    {
+        _lerpYPanCoroutine = StartCoroutine(LerpYAction(isPlayerFalling));
+    } // end LerpYDamping
+
+    private IEnumerator LerpYAction(bool isPlayerFalling)
+    {
+        IsLerpingYDamping = true;
+        // Grab the starting damping amount
+        float startDampAmount = _framingTransposer.m_YDamping;
+        float endDampAmount;
+        // Determine the end damping amount
+        if (isPlayerFalling)
+        {
+            endDampAmount = _fallPanAmount;
+            LerpedFromPlayerFalling = true;
+        }
+        else
+        {
+            endDampAmount = _normYPanAmount;
+        }
+
+        // Lerp the pan amount
+        float elapsedTime = 0f;
+        while (elapsedTime < _fallYPanTime)
+        {
+            elapsedTime += Time.deltaTime;
+
+            float LerpedPanAmount = Mathf.Lerp(startDampAmount, endDampAmount, elapsedTime / _fallYPanTime);
+            _framingTransposer.m_YDamping = LerpedPanAmount;
+            yield return null;
+        }
+        IsLerpingYDamping = false;
+    } // end LerpYAction
+
+    #endregion
+
+    #region Pan Camera
+    public void PanCameraOnContact(float panDistance, float panTime, PanDirection panDirection, bool panToStartingPos)
+    {
+        _panCameraCoroutine = StartCoroutine(PanCamera(panDistance, panTime, panDirection, panToStartingPos));
+    }
+    private IEnumerator PanCamera(float panDistance, float panTime, PanDirection panDirection, bool panToStartingPos)
+    {
+        Vector2 endPos = Vector2.zero;
+        Vector2 startingPos;
+        //set the direction and distance if we are panning in the direction indicated by the trigger object
+        if (!panToStartingPos)
+        {
+            //set the direction and distance
+            switch (panDirection)
+            {
+                case PanDirection.Up:
+                    endPos = Vector2.up;
+                    break;
+                case PanDirection.Down:
+                    endPos = Vector2.down;
+                    break;
+                case PanDirection.Left:
+                    endPos = Vector2.right;
+                    break;
+                case PanDirection.Right:
+                    endPos = Vector2.left;
+                    break;
+                default:
+                    break;
+            }
+            endPos *= panDistance;
+            startingPos = _startingTrackedObjectOffset;
+            endPos += startingPos;
+        }
+        // Handle the direction settings when moving back to the starting position
+        else
+        {
+            startingPos = _framingTransposer.m_TrackedObjectOffset;
+            endPos = _startingTrackedObjectOffset;
+            // Handle the actual panning of the camera
+            float elapsedTime = 0f;
+            while (elapsedTime < panTime)
+            {
+                elapsedTime += Time.deltaTime;
+                Vector3 panLerp = Vector3.Lerp(startingPos, endPos, elapsedTime / panTime);
+                _framingTransposer.m_TrackedObjectOffset = panLerp;
+                yield return null;
+            }
+        }
+    }
+    #endregion
+
+    #region Swap Cameras
+    public void SwapCamera(CinemachineVirtualCamera cameraFromLeft, CinemachineVirtualCamera cameraFromRight, Vector2 triggerExitDirection)
+    {
+        // If the current camera is the camera on the left and our trigger exit direction was on the right
+        if (_currentCamera == cameraFromLeft && triggerExitDirection.x > 0f)
+        {
+            // Activate the new camera
+            cameraFromRight.enabled = true;
+            // Deactivate the old camera
+            cameraFromLeft.enabled = false;
+            // Set the new camera as the current camera
+            _currentCamera = cameraFromRight;
+            //update our composer variable
+            _framingTransposer = _currentCamera.GetCinemachineComponent<CinemachineFramingTransposer>();
+        }
+        // If the currnet camera is the camera on theright and our trigger hit direction was on the left
+        else if (_currentCamera == cameraFromRight && triggerExitDirection.x < 0f)
+        {
+            // Activate the new camera
+            cameraFromLeft.enabled = true;
+            // Deactivate the old camera
+            cameraFromRight.enabled = false;
+            // Set the new camera as the current camera
+            _currentCamera = cameraFromRight;
+            // Update our composer variable
+            _framingTransposer = _currentCamera.GetCinemachineComponent<CinemachineFramingTransposer>();
+        }
+    } // end SwapCamera
+    #endregion
+} // end class CameraManager
