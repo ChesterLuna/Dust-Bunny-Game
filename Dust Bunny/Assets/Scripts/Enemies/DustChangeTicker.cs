09ead0fe51eddf9f2725c780ca2e37e2045e2b6f
Matthew Davies <70969486+mfdavies@users.noreply.github.com>
2024-03-26 22:07:50 -0600

    Fixed Cloud animation

--- Dust Bunny/Assets/Scripts/Enemies/DustChangeTicker.cs
+++ Dust Bunny/Assets/Scripts/Enemies/DustChangeTicker.cs
@@ -11,7 +11,7 @@ public class DustChangeTicker : MonoBehaviour
     [SerializeField] float _maxDustToExchange = 100;
     [Tooltip("The amount of dust to give/take each tick, can be positive or negative")]
     [SerializeField] float _dustTickAmount = 10;
-    float _amountOfDust = 0;
+    [SerializeField] float _amountOfDust = 0;
     IPlayerController _player;
 
     [Tooltip("How often the dust will be given/taken (in seconds).")]
@@ -20,15 +20,22 @@ public class DustChangeTicker : MonoBehaviour
     [SerializeField] float _cooldownTime = 5f;
     Collider2D _collider;
     SpriteRenderer _spriteRenderer;
+    [SerializeField] Sprite _small;
+    [SerializeField] Sprite _mid;
+    [SerializeField] Sprite _large;
+
+    private IEnumerator coroutine;
+
 
     private void Awake()
     {
         _collider = GetComponent<Collider2D>();
-        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
         if (_dustTickAmount == 0)
         {
             Debug.LogError("Dust tick amount is 0, this will cause the dust to never change");
         }
+        UpdateSprite();
     } // end Awake
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -36,7 +43,8 @@ public class DustChangeTicker : MonoBehaviour
         other.gameObject.TryGetComponent(out _player);
         if (_player != null)
         {
-            StartCoroutine(TryChangeDust());
+            coroutine = TryChangeDust();
+            StartCoroutine(coroutine);
         }
     } // end OnTriggerEnter2D
 
@@ -45,7 +53,7 @@ public class DustChangeTicker : MonoBehaviour
         other.gameObject.TryGetComponent(out IPlayerController player);
         if (player == _player)
         {
-            CancelInvoke(nameof(TryChangeDust));
+            StopCoroutine(coroutine);
             _player = null;
         }
     } // end OnTriggerExit2D
@@ -63,6 +71,7 @@ public class DustChangeTicker : MonoBehaviour
             else
             {
                 _player?.ChangeDust(_dustTickAmount, true);
+                UpdateSprite();
                 if ((Mathf.Abs(_amountOfDust) == _maxDustToExchange && _maxDustToExchange != -1) || _dustTickRate == -1)
                 {
                     break;
@@ -92,6 +101,31 @@ public class DustChangeTicker : MonoBehaviour
         _amountOfDust = 0;
         _collider.enabled = active;
         if (_spriteRenderer != null) _spriteRenderer.enabled = active;
+        UpdateSprite();
     } // end SetActive
 
+    private void UpdateSprite()
+    {
+        if (_spriteRenderer == null) return;
+        if (_maxDustToExchange == -1)
+        {
+            _spriteRenderer.sprite = _large;
+        }
+        else
+        {
+            float percentage = Mathf.Abs(_amountOfDust) / _maxDustToExchange * 100;
+            if (percentage < 33)
+            {
+                _spriteRenderer.sprite = _large;
+            }
+            else if (percentage >= 33 && percentage < 66)
+            {
+                _spriteRenderer.sprite = _mid;
+            }
+            else
+            {
+                _spriteRenderer.sprite = _small;
+            }
+        }
+    } // end SetAnimation
 } // end class DustRemoval
