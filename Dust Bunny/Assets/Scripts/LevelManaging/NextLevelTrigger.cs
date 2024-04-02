4f75f617e74a156b53de4cd58e780d99ac88031d
ChesterLuna <chester99124@gmail.com>
2024-04-01 21:47:50 -0600

    Usable for changing scenes in animations

--- Dust Bunny/Assets/Scripts/LevelManaging/NextLevelTrigger.cs
+++ Dust Bunny/Assets/Scripts/LevelManaging/NextLevelTrigger.cs
@@ -9,15 +9,21 @@ public class NextLevelTrigger : MonoBehaviour
     [SerializeField] private Vector3 _nextLevelSpawnLocation;
     [SerializeField] private Animator _transition;
     [SerializeField] private float _transitionTime = 1f;
+    [SerializeField] private bool _onTouch = true;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && _onTouch)
         {
-            GameManager.instance.CheckpointLocation = _nextLevelSpawnLocation;
-
-            LevelLoader levelLoader = FindObjectOfType<LevelLoader>();
-            levelLoader.StartLoadLevel(_nextLevelName, _transition, _transitionTime);
+            ChangeScene();
         }
     } // end OnTriggerEnter2D
+
+    public void ChangeScene()
+    {
+        GameManager.instance.CheckpointLocation = _nextLevelSpawnLocation;
+
+        LevelLoader levelLoader = FindObjectOfType<LevelLoader>();
+        levelLoader.StartLoadLevel(_nextLevelName, _transition, _transitionTime);
+    }
 } // end class NextLevelTrigger
