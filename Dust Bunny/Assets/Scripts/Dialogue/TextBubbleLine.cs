efa7fa59043a030d2a911219da2f9ae2ed8ba94e
Alikay <asalm@ualberta.ca>
2024-07-05 03:30:19 -0600

    added an outline to speech bubble and made it behave better when moving

--- Dust Bunny/Assets/Scripts/Dialogue/TextBubbleLine.cs
+++ Dust Bunny/Assets/Scripts/Dialogue/TextBubbleLine.cs
@@ -6,17 +6,20 @@ using UnityEngine;
 
 public class TextBubbleLine : MonoBehaviour
 {
-    Mesh m;
-    MeshFilter mf;
+    MeshFilter[] meshes;
 
     private GameObject currentTarget;
 
     // Use this for initialization
     void Start()
     {
-        mf = GetComponent<MeshFilter>();
-        m = new Mesh();
-        mf.mesh = m;
+        meshes = GetComponentsInChildren<MeshFilter>();
+
+        foreach(MeshFilter meshFilter in meshes){
+            Mesh m = new Mesh();
+            meshFilter.mesh = m;
+        }
+        
     }
 
     void Update(){
@@ -39,40 +42,43 @@ public class TextBubbleLine : MonoBehaviour
     //This draws a triangle
     void DrawLineToPoint(GameObject target)
     {
-        //We need two arrays one to hold the vertices and one to hold the triangles
-        Vector3[] VerteicesArray = new Vector3[3];
-        int[] trianglesArray = new int[3];
+        foreach(MeshFilter mf in meshes){
+            Mesh m = mf.mesh;
+            //We need two arrays one to hold the vertices and one to hold the triangles
+            Vector3[] VerteicesArray = new Vector3[3];
+            int[] trianglesArray = new int[3];
 
-        //lets add 3 vertices in the 3d space
+            //lets add 3 vertices in the 3d space
 
-        // find the position of the target
-        Vector3 targetPos = transform.position;
-        if(target != null){
-            targetPos = target.transform.position;
-            BoxCollider2D box = target.GetComponent<BoxCollider2D>();
-            if(box != null && target.name == "SpeechBubbleTarget"){
-                targetPos = box.ClosestPoint(transform.position);
+            // find the position of the target
+            Vector3 targetPos = transform.position;
+            if(target != null){
+                targetPos = target.transform.position;
+                BoxCollider2D box = target.GetComponent<BoxCollider2D>();
+                if(box != null && target.name == "SpeechBubbleTarget"){
+                    targetPos = box.ClosestPoint(transform.position);
+                }
             }
+
+            // find the position of the two other points
+            VerteicesArray[0] = new Vector3(
+                Mathf.Sin(Mathf.Deg2Rad * Vector3.Angle(targetPos, transform.position) + (Mathf.PI/4)), 
+                Mathf.Cos(Mathf.Deg2Rad * Vector3.Angle(targetPos, transform.position) + (Mathf.PI/4)),
+            0);
+            VerteicesArray[1] = targetPos - transform.position;
+            VerteicesArray[2] = new Vector3(
+                Mathf.Sin(Mathf.Deg2Rad * (Vector3.Angle(targetPos, transform.position)) - (Mathf.PI/4)), 
+                Mathf.Cos(Mathf.Deg2Rad * Vector3.Angle(targetPos, transform.position) - (Mathf.PI/4)),
+            0);
+
+            //define the order in which the vertices in the VerteicesArray shoudl be used to draw the triangle
+            trianglesArray[0] = 0;
+            trianglesArray[1] = 1;
+            trianglesArray[2] = 2;
+
+            //add these two triangles to the mesh
+            m.vertices = VerteicesArray;
+            m.triangles = trianglesArray;
         }
-
-        // find the position of the two other points
-        VerteicesArray[0] = new Vector3(
-            Mathf.Sin(Vector3.Angle(targetPos, transform.position) + (Mathf.PI/4)), 
-            Mathf.Cos(Vector3.Angle(targetPos, transform.position) + (Mathf.PI/4)),
-        0);
-        VerteicesArray[1] = targetPos - transform.position;
-        VerteicesArray[2] = new Vector3(
-            Mathf.Sin(Vector3.Angle(targetPos, transform.position) - (Mathf.PI/4)), 
-            Mathf.Cos(Vector3.Angle(targetPos, transform.position) - (Mathf.PI/4)),
-        0);
-
-        //define the order in which the vertices in the VerteicesArray shoudl be used to draw the triangle
-        trianglesArray[0] = 2;
-        trianglesArray[1] = 1;
-        trianglesArray[2] = 0;
-
-        //add these two triangles to the mesh
-        m.vertices = VerteicesArray;
-        m.triangles = trianglesArray;
     }
 }
